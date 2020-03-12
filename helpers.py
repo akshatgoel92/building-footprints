6e2e069e3d78d8bcc0a2206016f7b0fd52bd0695
akshatgoel92 <akshatgoel92@hotmail.com>
2020-03-12 15:18:26 +0530

    Add finalized tested S3 helpers file

--- helpers.py
+++ helpers.py
@@ -6,64 +6,53 @@ import argparse
 import numpy as np
 import pandas as pd
     
-    
-def get_object_s3(key):
+
+def get_credentials():
     '''
-    Get an object from S3.
+    Get credentials for S3 access.
     '''
-    
     with open('./secrets.json') as secrets:
         s3_access = json.load(secrets)['s3']
     
-    bucket_name = s3_access['default_bucket']
-    access_key_id = s3_access['access_key_id']
-    secret_access_key = s3_access['secret_access_key']
-    
-    s3 = boto3.client('s3',access_key_id, secret_access_key)
-    response = s3.get_object(bucket_name, key)
-    file = response["Body"]
-    
-    return(file)
-    
-    
-def upload_s3(file_from, file_to):
+    return(s3_access['default_bucket'], 
+           s3_access['access_key_id'],
+           s3_access['secret_access_key'])
+
+
+def get_s3_client():
     '''
-    Upload an object to S3.
+    Get an S3 client object.
     '''
+    _, access_key_id, secret_access_key = get_credentials()
+    s3 = boto3.client("s3", 
+                      aws_access_key_id = access_key_id, 
+                      aws_secret_access_key = secret_access_key)
     
-    with open('./secrets.json') as secrets:
-        s3_access = json.load(secrets)['s3']
-    
-    bucket_name = s3_access['default_bucket']
-    access_key_id = s3_access['access_key_id']
-    secret_access_key = s3_access['secret_access_key']
-    
-    s3 = boto3.client('s3', access_key_id, secret_access_key)
-    s3.upload_file(file_from, bucket_name, file_to)
-    
-    
-def download_file_s3(file_from = '/Image Folder/Deoria Google Earth Image/AOI_GE_Image_1_1.tif',
-                     file_to = './AOI_GE_Image_1_1.tif'):
+    return(s3)
+
+
+def get_s3_resource():
     '''
-    Download a particular file from S3.
+    Get an S3 resource object.
     '''
-    
-    with open('./secrets.json') as secrets:
-        s3_access = json.load(secrets)['s3']
-    
-    bucket_name = s3_access['default_bucket']
-    access_key_id = s3_access['access_key_id']
-    secret_access_key = s3_access['secret_access_key']
-    
+    _, access_key_id, secret_access_key = get_credentials()
     s3 = boto3.resource('s3', 
                         aws_access_key_id = access_key_id, 
                         aws_secret_access_key = secret_access_key)
-    try:
-        s3.Bucket(bucket_name).download_file(file_from, file_to)
-    except Exception as e: 
-        print(e)
     
+    return(s3)
+
+
+def get_bucket_name():
+    '''
+    Get the default bucket name.
+    '''
+    bucket_name, _, _ = get_credentials()
     
+    return(bucket_name)
+
+    
+
 def get_matching_s3_objects(prefix="", suffix=""):
     """
     Generate objects in an S3 bucket.
@@ -72,21 +61,14 @@ def get_matching_s3_objects(prefix="", suffix=""):
     Taken from: https://alexwlchan.net/2019/07/listing-s3-keys/
     Copyright © 2012–19 Alex Chan. Prose is CC-BY licensed, code is MIT.
     """
-    with open('./secrets.json') as secrets:
-        s3_access = json.load(secrets)['s3']
-        
-    bucket_name = s3_access['default_bucket']
-    access_key_id = s3_access['access_key_id']
-    secret_access_key = s3_access['secret_access_key']
-    
-    s3 = boto3.client("s3", access_key_id, secret_access_key)
+    s3 = get_s3_client()
+    kwargs = {'Bucket': get_bucket_name()} 
     paginator = s3.get_paginator("list_objects_v2")
-    kwargs = {'Bucket': bucket_name}
-    	
-    # We can pass the prefix directly to the S3 API.  
-    # If the user has passed a tuple or list of prefixes, we go through them one by one.
-    if isinstance(prefix, str): prefixes = (prefix, )
-    else: prefixes = prefix
+       	
+    if isinstance(prefix, str): 
+        prefixes = (prefix, )
+    else: 
+        prefixes = prefix
     
     for key_prefix in prefixes: 
         kwargs["Prefix"] = key_prefix
@@ -110,10 +92,60 @@ def get_matching_s3_keys(prefix="", suffix=""):
     Taken from: https://alexwlchan.net/2019/07/listing-s3-keys/
     Copyright © 2012–19 Alex Chan. Prose is CC-BY licensed, code is MIT.
     """
-    for obj in get_matching_s3_objects(prefix, suffix): yield obj["Key"]
+    for obj in get_matching_s3_objects(prefix, suffix): 
+        yield obj["Key"]
+
+
+def get_object_s3(key):
+    '''
+    Get an object from S3.
+    '''
+    s3 = get_s3_client()
+    bucket_name = get_bucket_name()
+    f = s3.get_object(bucket_name, key)["Body"]
+    
+    return(f)
+    
+        
+def download_s3(file_from = 'Raw Data/NRSC_Raw/198124711/ACC_REP.txt',
+                file_to = './test.txt'):
+    '''
+    Download a particular file from S3.
+    '''
+    s3 = get_s3_resource()
+    bucket_name = get_bucket_name()
+    try:
+        s3.Bucket(bucket_name).download_file(file_from, file_to)
+    except Exception as e: 
+        print(e)
+
+
+def upload_s3(file_from = './test.txt', 
+              file_to = 'Raw Data/NRSC_Raw/198124711/test.txt' ):
+    '''
+    Upload an object to S3.
+    '''
+    
+    s3 = get_s3_client()
+    bucket_name = get_bucket_name()
+    s3.upload_file(file_from, bucket_name, file_to)
+
+def tests():
+    
+    get_credentials()
+    get_bucket_name()
+    get_s3_client()
+    
+    get_matching_s3_objects()
+    get_matching_s3_keys()
+    download_s3()
+    upload_s3()
     
     
 if __name__ == '__main__':
-    
-    main()
+    '''
+    Function testing happens here. 
+    '''
+    tests()
+   
     
