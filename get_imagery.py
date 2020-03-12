a9cce319916149a325078ab62ef47c6f0562f56c
akshatgoel92 <akshatgoel92@hotmail.com>
2020-03-12 16:07:43 +0530

    Add first pass at imagery to get script from S3 console

---
+++ get_imagery.py
@@ -0,0 +1,54 @@
+# Import packages
+import os 
+import json
+import boto3
+import argparse
+import numpy as np
+import pandas as pd
+
+# Import S3 wrappers
+def make_image_folder():
+    
+    pass
+
+
+def get_google_earth():
+    
+    pass
+
+
+def get_landsat_30m():
+    
+    pass
+
+
+def get_metal_shapefile():
+    
+    pass
+
+
+def get_nrsc_5m():
+    
+    pass
+
+
+def get_sentinel_10m():
+    
+    pass
+
+
+def get_ghaziabad_ge():
+    
+    pass
+
+
+def main():
+    
+    pass
+
+
+if __name__ '__main__':
+    
+    pass
+
+ 
