e032265162494348acbfcaf160d2bc48af9e8e5b
akshatgoel92 <akshatgoel92@hotmail.com>
2020-03-18 12:42:09 +0530

    Add next pass at loading vector file

--- get_vector.py
+++ get_vector.py
@@ -1,18 +1,28 @@
 import fiona
-
+import argparse
 
 def open_shape_file(path):
     
     f = fiona.open(path, 'r')
     
     return(f)
+
+def parse_args():
     
+    parser = argparse.ArgumentParser(description ='Data loading parser')
+    
+    return(path)
+
 
 def tests():
     
-    test = open_shape_file('./Image Folder/Deoria Metal Shapefile/Metal roof.shp')
-    return(test)
+    test = './Image Folder/Deoria Metal Shapefile/Metal roof.shp'
+    path = parse_args()
+    assert path == test
+    
+    shp = open_shape_file(path)
+    return(shp)
 
 if __name__ == '__main__':
     
-    test = tests()
+    shp = tests()
