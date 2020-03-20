7266fd7d0b230405218b81600c4b23d87bff0dfc
akshatgoel92 <akshatgoel92@hotmail.com>
2020-03-20 18:53:00 +0530

    Add command line arguments to mask generator - still need to test

--- summarize/get_masks.py
+++ summarize/get_masks.py
@@ -2,19 +2,21 @@ from helpers import raster
 from helpers import vector 
 from helpers import common 
 
+import os
 import fiona
 import rasterio
 import rasterio.mask
 
 
-def get_masks(root, image_type, image_name):
+def get_masks(root, image_type, image_name, shape):
     '''
     ------------------------
     Input: 
     Output:
     ------------------------
     '''
-    shapes = vector.get_shapes()
+    shapes = vector.\
+             get_shapes(**shape)
     
     with raster.\
     get_image(root, image_type, image_name) as image:
@@ -57,6 +59,27 @@ def write_masks(out_image, out_transform,
            out_transform, 
            out_meta)
 
+
+def parse_args():
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--root', type = str, default = 'Sentinel', required = False)
+    parser.add_argument('--image_type', type = str, default= 'Gorakhpur', required = False)
+    parser.add_argument('--image_name', type = int, default = 'RT_T44RQQ_20190521T045701_B02.tif', required = False)
+    
+    parser.add_argument('--shape_root', type = str, default = 'Metal Shapefile', required = False)
+    parser.add_argument('--shape_image_type', type = str, default= 'Gorakhpur', required = False)
+    parser.add_argument('--shape_image_name', type = int, default = 'Metal roof.shp', required = False)
+    args = parser.parse_args()
+    
+    root = args.root
+    image_type = args.image_type
+    image_name = args.image_name
+    shape_root = args.shape_root
+    shape_type = args.shape_type
+    shape_name = args.shape_name
+    mask_name = os.path.splitext(image_name)[0]
+    
 if __name__ == '__main__':
     
     out_image, out_transform, out_meta = get_masks()
