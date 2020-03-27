ae8a24b3759d8123eab05a6f63a942de22662ec9
akshatgoel92 <akshatgoel92@hotmail.com>
2020-03-27 14:15:51 +0530

    Add summary statistics calculators

--- helpers/raster.py
+++ helpers/raster.py
@@ -6,8 +6,8 @@ import rasterio
 import numpy as np
 import matplotlib.pyplot as plt
 
-from rasterio.plot import show
 from helpers import common
+from rasterio.plot import show
 
 
 def list_images(root, image_type):
@@ -23,7 +23,8 @@ def list_images(root, image_type):
     
     images = [f for f in os.listdir(path) 
               if os.path.\
-              isfile(os.path.join(path, f))]
+              isfile(os.path.join(path, f))
+              and f.endswith('.tif')]
     
     return(images)
 
@@ -80,3 +81,61 @@ def write_image(root, image_type,
     
     with rasterio.open(out_path, "w", **out_meta) as dest:
         dest.write(out_image)
+
+
+def get_resolution(img):
+    '''
+    ------------------------
+    Input: 
+    Output:
+    ------------------------
+    '''
+    return(img.res)
+
+
+def get_total_tiles(root, image_type):
+    '''
+    ------------------------
+    Input: 
+    Output:
+    ------------------------
+    '''
+    return(len(list_images(root, image_type)))
+
+
+def get_average(arr):
+    '''
+    ------------------------
+    Input: 
+    Output:
+    ------------------------
+    '''
+    avgs = [band.mean() for band in arr]
+
+    return(avgs)
+
+
+def get_sd(arr):
+    '''
+    ------------------------
+    Input: 
+    Output:
+    ------------------------
+    '''
+    sd = [band.std() for band in arr]
+
+    return(sd)
+
+
+def get_area(image):
+    '''
+    ------------------------
+    Input: 
+    Output:
+    ------------------------
+    '''
+    length = image.bounds.top - image.bounds.bottom 
+    width = image.bounds.right - image.bounds.left
+    area = (length*width)/(1000*1000)
+    
+    return(area)
--- helpers/vector.py
+++ helpers/vector.py
@@ -1,5 +1,18 @@
+# Import packages
 import fiona
+import pyproj
+import numpy as np
+
+# Import sub-modules for area calculation
+from area import area
 from helpers import common
+from helpers import raster
+from functools import partial
+from shapely.ops import transform
+from shapely.geometry import shape
+
+
+
 
 
 def open_shape_file(path):
@@ -14,24 +27,44 @@ def open_shape_file(path):
     return(f)
 
 
-def get_shapes(root, image_type, image_name):
+def get_shapes(shape):
     '''
     ------------------------
     Input: 
     Output:
     ------------------------
     '''
-    path = common.\
-           get_local_image_path(root, 
-                                image_type, 
-                                image_name)
-    
-
-    with fiona.open(path, 'r') as shapefile:
-        # Need to be careful that no None objects 
-        # are in the shapefile
-        shapes = [feature["geometry"] 
-                  for feature in shapefile 
-                  if feature["geometry"] is not None]
+    shapes = [feature["geometry"] 
+              for feature in shape 
+              if feature["geometry"] is not None]
     
     return(shapes)
+
+
+def get_poly_area(shapes):
+    '''
+    ------------------------
+    Input: 
+    Output:
+    Source: https://github.com/scisco/area
+    ------------------------
+    '''
+    areas = np.array([area(obj) for obj in shapes]).sum()
+    areas = areas/(1000*1000)
+    
+    return(areas)
+    
+
+
+def calculate_area_proportion(shapes, path):
+    '''
+    ------------------------
+    Input: 
+    Output:
+    ------------------------
+    '''
+    vec = vector.get_area(shapes)
+    ras = raster.get_area(path)
+    proportion = (vec/ras)*100
+    
+    return(proportion)
