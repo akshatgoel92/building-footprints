a8e7dc6da4a9241871a7f64dfa5d09686e6d9813
akshatgoel92 <akshatgoel92@hotmail.com>
2020-04-04 03:01:28 +0530

    Add finalized pass at logistic regression

--- models/log_reg.py
+++ models/log_reg.py
@@ -1,19 +1,90 @@
-# logistic regression with L1 and L2 regularization
-from keras.regularizers import l1l2
-from keras.models import Sequential
-from keras.layers import Dense
+# Import packages
+from sklearn.linear_model import LogisticRegression
+from helpers import common
+from models import utils
+
+# 
+def load_data(root, image_type):
+    '''
+    -------------------
+    Input:
+    Output:
+    -------------------
+    '''
+    path = common.get_s3_paths(root, image_type)
+    f = [get_object_s3(path, image) for image in images]
+    
 
 
-def load_data():
+def merge_data():
+    '''
+    -------------------
+    Input:
+    Output:
+    -------------------
+    '''
     pass
 
 
-def model_compile():
-    # 2-class logistic regression in Keras
-    model = Sequential()
-    model.add(Dense(1, activation="sigmoid", input_dim=x.shape[1]))
-    model.compile(optimizer="rmsprop", loss="binary_crossentropy")
+
+def get_estimator(c):
+    '''
+    -------------------
+    Input:
+    Output:
+    -------------------
+    '''
+    log_reg100 = LogisticRegression(C=100)
+    log_reg100.fit(X_train, y_train)
 
 
-def model_fit():
-    model.fit(x, y, nb_epoch=10, validation_data=(x_val, y_val))
+
+def train():
+    '''
+    -------------------
+    Input:
+    Output:
+    -------------------
+    '''
+    pass
+
+
+def get_predictions(log_reg):
+    '''
+    -------------------
+    Input:
+    Output:
+    -------------------
+    '''
+    pass
+
+
+def get_scores(log_reg):
+    '''
+    -------------------
+    Input:
+    Output:
+    -------------------
+    '''
+    train_scores = []
+    test_scores = []
+    
+    train_scores.append(log_reg.score(X_train, y_train))
+    test_scores.append(log_reg.score(X_test, y_test))
+    
+
+
+def main():
+    '''
+    -------------------
+    Input:
+    Output:
+    -------------------
+    '''
+    []
+
+
+
+if __name__ == '__main__':
+    main()
+
