497b30d66c09b75f0f234c151a6e5a5513bcf1f8
akshatgoel92 <akshatgoel92@hotmail.com>
2020-04-05 00:01:19 +0530

    Reduce parallel workers from 12 to 4

--- models/random_forest.py
+++ models/random_forest.py
@@ -120,7 +120,7 @@ def fit(X, Y, n_estimators):
     Output:
     -------------------
     '''
-    rf_reg = RandomForestClassifier(n_estimators = n_estimators, n_jobs = -1, verbose = 1000)
+    rf_reg = RandomForestClassifier(n_estimators = n_estimators, n_jobs = 4, verbose = 1000)
     rf_reg.fit(X, Y)
     
     return(log_reg)
