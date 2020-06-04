39c4ec3a15f51395708cf3d34d82c58b885ebf1a
akshatgoel92 <akshatgoel92@hotmail.com>
2020-06-04 16:52:05 +0530

    Add folder for tests

---
+++ tests/local.py
@@ -0,0 +1,31 @@
+from predict import predict
+from train import train
+from unet import unet
+    
+    
+def predict():
+    pass
+    
+    
+def train():
+    pass
+    
+    
+def unet():
+    pass
+    
+    
+def flatten():
+    pass
+    
+    
+def prep_img():
+    pass
+    
+    
+def main():
+    pass
+    
+    
+if __name__ == '__main__':
+    main()
