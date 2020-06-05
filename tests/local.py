b764eb6a29ee702893442ba63e64b92fb1c11dc5
akshatgoel92 <akshatgoel92@hotmail.com>
2020-06-05 16:46:52 +0530

    Add local test for new custom data generator

--- tests/local.py
+++ tests/local.py
@@ -1,4 +1,10 @@
 from predict import predict
+from train import train
+
+from unet import datagen
+from unet import utils
+
+
     
     
 def test_predict():
@@ -15,8 +21,36 @@ def test_predict():
     except Exception as e:
         print("Got an error!")
         print(e)
+        
+        
+def test_datagen(model_type = "unet"):
+    """
+    ---------------------------------------------
+    Input: None
+    Output: None
+    Run the test harness for evaluating a model
+    ---------------------------------------------
+    """
+    settings = utils.get_settings(model_type)
+    load_dataset_args = settings['load_dataset_args']
     
+    path_args=  settings['path_args']
+    paths = utils.get_paths(**path_args)
+    train, val = datagen.load_dataset(paths, load_dataset_args)
     
+    try:
+        train_img = next(train)
+    except Exception as e:
+        print("Got an error!")
+        print(e)
+    
+    try: 
+        val_img = next(val)
+    except Exception as e:
+        print("Got an error!")
+        print(e)
+            
+        
 def main():
     """
     ---------------------------------------------
@@ -25,7 +59,7 @@ def main():
     Run the test harness for evaluating a model
     ---------------------------------------------
     """
-    test_predict()
+    test_datagen()
     
     
 if __name__ == '__main__':
